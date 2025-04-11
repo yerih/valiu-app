5f6f783e07df71f01ac457ad4b9943d9ed4386f6
yerih <yerih7tup@gmail.com>
2025-04-11 12:12:09 +0200

    CustomScrollView is added. SliverAppBar is added.

--- lib/presentation/screens/homeScreen.dart
+++ lib/presentation/screens/homeScreen.dart
@@ -17,34 +17,91 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        drawer: drawer,//CustomDrawer(),
-        body: Builder(
-          builder: (BuildContext context) {
-            return BlurredBackground(
-              imagePath: 'assets/images/img.png',
-              child: SizedBox.expand(
-                child: Column(
+
+    return Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              leading: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.menu),
+              ),
+              pinned: true,
+              expandedHeight: 400,
+              backgroundColor: Colors.black,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Column(
                   mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Center', style: textDrawerItem),
-                    Text('Center', style: textDrawerItem),
-                    ElevatedButton(
-                      onPressed: () {
-                        Scaffold.of(context).openDrawer();
-                      },
-                      child: const Text('Open Drawer'),
+                    Text('Featured', style: textDrawerItem),
+                    Container(
+                      margin: const EdgeInsets.only(top: 4.0),
+                      height: 3.0,
+                      width: 60,// Adjust width as needed
+                      decoration: BoxDecoration(
+                        color: Colors.white, // Or your preferred color
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+                      ),
                     ),
                   ],
+                ),
+                // title: Text('Featured', style: textDrawerItem),
+                centerTitle: false,
+                titlePadding: const EdgeInsets.only(left: 20),
+                collapseMode: CollapseMode.parallax,
+                background: ClipRRect(
+                  borderRadius: BorderRadius.only(bottomRight: Radius.circular(20)),
+                  child: Image(
+                    image: AssetImage('assets/images/img.png'),
+                    fit: BoxFit.cover,
+                  )
                 )
-              )
-            );
-
-          }
-        ),
-      )
+              ),
+            ),
+            SliverList(
+                delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                      return ListTile(
+                        title: Text('Item $index'),
+                      );
+                    },
+                    childCount: 100
+                )
+            )
+          ],
+        )
     );
+
+
+    // return MaterialApp(
+    //   home: Scaffold(
+    //     drawer: drawer,//CustomDrawer(),
+    //     body: Builder(
+    //       builder: (BuildContext context) {
+    //         return BlurredBackground(
+    //           imagePath: 'assets/images/img.png',
+    //           child: SizedBox.expand(
+    //             child: Column(
+    //               mainAxisSize: MainAxisSize.min,
+    //               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //               children: [
+    //                 Text('Center', style: textDrawerItem),
+    //                 Text('Center', style: textDrawerItem),
+    //                 ElevatedButton(
+    //                   onPressed: () {
+    //                     Scaffold.of(context).openDrawer();
+    //                   },
+    //                   child: const Text('Open Drawer'),
+    //                 ),
+    //               ],
+    //             )
+    //           )
+    //         );
+    //
+    //       }
+    //     ),
+    //   )
+    // );
   }
 }
--- lib/presentation/styles/StyleText.dart
+++ lib/presentation/styles/StyleText.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 const TextStyle textDrawerItem = TextStyle(
   color: Colors.white,
-  fontSize: 13.5,
+  fontSize: 20,
 );
 
 
